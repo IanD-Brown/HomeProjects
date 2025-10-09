ea424a0aabf149758363387e36de90b8e0d39ffd
IanD-Brown <ian_d_brown@yahoo.com>
2025-10-09 15:17:02 +0100

    day 6

---
+++ src/main/kotlin/Day6Solver.kt
@@ -0,0 +1,15 @@
+package org.idb
+
+class Day00Solver : DaySolver<Int, Int>() {
+    override fun loadData(lines : Sequence<String>) {
+        TODO()
+    }
+
+    override fun calcPart1() : Int {
+        TODO()
+    }
+
+    override fun calcPart2(): Int {
+        TODO()
+    }
+}
